73f47aa5a471bac7ca523467a5e4322c5b9a9e51
SymbolixAU <dcooley@symbolix.com.au>
2018-09-30 20:20:35 +1000

    documentation '

--- R/to_json.R
+++ R/to_json.R
@@ -3,57 +3,65 @@
 #' Converts a data.frame to JSON
 #' 
 #' @param x object to convert to JSON
+#' @export
+to_json <- function( x, ... ) UseMethod("to_json")
+
+#' @rdname to_json
+#' @export
 #' @param numeric_dates logical indicating if dates should be treated as numerics. 
 #' Defaults to TRUE for speed. If FALSE, the dates will be coerced to character
-#' @export
-to_json <- function( x, numeric_dates = TRUE ) {
-  #if(!numeric_dates) x <- handle_dates( x ) 
-  convert_to_json( x, numeric_dates )
-}
-
-convert_to_json <- function( x, numeric_dates = TRUE ) UseMethod("convert_to_json") 
-
-#' @export
-convert_to_json.data.frame <- function( x, numeric_dates = TRUE ) {
+to_json.data.frame <- function( x, numeric_dates = TRUE ) {
   if(!numeric_dates) x <- handle_dates( x ) 
   rcpp_df_to_json( x )
 }
 
+#' @rdname to_json
 #' @export
-convert_to_json.numeric <- function( x, ... ) rcpp_numeric_to_json( x )
+to_json.numeric <- function( x, ... ) rcpp_numeric_to_json( x )
 
+#' @rdname to_json
 #' @export
-convert_to_json.character <- function( x, ... ) rcpp_character_to_json( x )
+to_json.character <- function( x, ... ) rcpp_character_to_json( x )
 
+#' @rdname to_json
 #' @export
-convert_to_json.integer <- function( x, ... ) rcpp_integer_to_json( x ) 
+to_json.integer <- function( x, ... ) rcpp_integer_to_json( x ) 
 
+#' @rdname to_json
 #' @export
-convert_to_json.logical <- function( x, ... ) rcpp_logical_to_json( x )
+to_json.logical <- function( x, ... ) rcpp_logical_to_json( x )
 
+#' @rdname to_json
 #' @export
-convert_to_json.Date <- function( x, numeric_dates = TRUE ) {
+#' @inheritParams convert_to_json.data.frame
+to_json.Date <- function( x, numeric_dates = TRUE ) {
   if( numeric_dates ) return( rcpp_numeric_to_json( x ) ) 
   return( rcpp_character_to_json( as.character( x ) ) )
 }
 
+#' @rdname to_json
 #' @export
-convert_to_json.POSIXct <- function( x, numeric_dates = TRUE ) {
+#' @inheritParams convert_to_json.data.frame
+to_json.POSIXct <- function( x, numeric_dates = TRUE ) {
   if( numeric_dates ) return( rcpp_numeric_to_json( x ) ) 
   return( rcpp_character_to_json( as.character( x ) ) )
 }
 
+#' @rdname to_json
 #' @export
-convert_to_json.POSIXlt <- function( x, numeric_dates = TRUE ) {
+#' @inheritParams convert_to_json.data.frame
+to_json.POSIXlt <- function( x, numeric_dates = TRUE ) {
   if( numeric_dates ) return( rcpp_list_to_json( x ) ) 
   return( rcpp_character_to_json( as.character( x ) ) )
 }
 
+#' @rdname to_json
 #' @export
-convert_to_json.list <- function( x, ... ) rcpp_list_to_json( x )
+to_json.list <- function( x, ... ) rcpp_list_to_json( x )
 
+#' @rdname to_json
 #' @export
-convert_to_json.default <- function( x, ... ) rcpp_list_to_json( x ) # stop("this type is not supported")
+to_json.default <- function( x, ... ) rcpp_list_to_json( x ) # stop("this type is not supported")
 
 date_columns <- function( df ) names(which(vapply(df , function(x) { inherits(x, "Date") || inherits(x, "POSIXct") }, T)))
 
--- tests/testthat/test-to_json_vectors.R
+++ tests/testthat/test-to_json_vectors.R
@@ -13,18 +13,12 @@ test_that("different vector types work", {
   expect_equal(as.character(to_json(as.POSIXlt("2018-01-01 01:00:00", tz = "GMT"))), "{\"sec\":[0.0],\"min\":[0],\"hour\":[1],\"mday\":[1],\"mon\":[0],\"year\":[118],\"wday\":[1],\"yday\":[0],\"isdst\":[0]}")
   expect_equal(as.character(to_json(as.POSIXct("2018-01-01 01:00:00", tz = "GMT"), numeric_dates = F)), "[\"2018-01-01 01:00:00\"]")
   expect_equal(as.character(to_json(complex(1))),"[\"0+0i\"]")
-  
   ## Numeric dates not implemented for lists
   expect_equal(as.character(to_json( list(x = as.Date("2018-01-01") ), numeric_dates = F)), '{"x":[17532.0]}')
   expect_equal(as.character(to_json( list(x = as.Date("2018-01-01") ), numeric_dates = T)), '{"x":[17532.0]}')
-  
 })
 
-
 test_that("NAs, NULLS and Infs work", {
-  
-  ## TODO ( should these all be JSON null values, rather than "NA" string?)
-  
   expect_equal(as.character(to_json( NA )), "[null]")
   expect_equal(as.character(to_json( NA_character_ )), "[null]")
   expect_equal(as.character(to_json( NA_complex_ )), "[null]")
@@ -36,10 +30,7 @@ test_that("NAs, NULLS and Infs work", {
   expect_equal(as.character(to_json(NULL)), "{}")
   expect_equal(as.character(to_json(list(a="a",b=NULL))),"{\"a\":[\"a\"],\"b\":{}}")
   expect_equal(as.character(to_json(data.frame())), "[]")
-  
 })
-
-
 
 test_that("round trips with jsonlite work", {
   x <- c(1L, NA_integer_)
@@ -57,3 +48,7 @@ test_that("round trips with jsonlite work", {
 })
 
 ## TODO( test list of all mixed types, inc Date, POSIXct and POSIXlt)
+
+# lst <- list(x = as.Date("2018-01-01"), y = list(as.POSIXct("2018-01-01 10:00:00")), z = NA)
+# as.character(to_json( lst, numeric_dates = T ))
+
