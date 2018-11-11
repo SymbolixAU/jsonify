e495d9d7747c75049afef8df40e0a3c792eb9c89
ChrisMuir <chrismuirRVA@gmail.com>
2018-11-11 02:11:16 -0500

    add support for NA/null values

--- inst/include/from_json/from_json.hpp
+++ inst/include/from_json/from_json.hpp
@@ -77,6 +77,12 @@ namespace from_json {
         }
         break;
       }
+      
+      // null
+      case 0: {
+        out[i] = Rcpp::CharacterVector(NA_STRING);
+        break;
+      }
         
       // array
       case 4: {
@@ -103,50 +109,31 @@ namespace from_json {
   // Can handle key/value pair JSON, and JSON that is missing keys.
   template<typename T>
   SEXP parse_array(T& array) {
-    bool list_out = false;
     int array_len = array.Size();
-    int data_type = array[0].GetType();
     
-    if(data_type == 2) {
-      data_type = 1;
-    }
+    // Check the data type of each value in the array.
+    Rcpp::IntegerVector dtypes = get_dtypes<T>(array);
+    Rcpp::IntegerVector u_dtypes = unique(dtypes).sort();
+    int data_type = u_dtypes[0];
     
-    if(data_type == 6) {
-      if(array[0].IsDouble()) {
-        data_type = 8;
-      } else {
-        data_type = 9;
-      }
-    }
-    
-    // Check to see if the array has different data types, which means return
-    // will be a list. Exception to this rule is for bool types...false has
-    // data type of 1, and true has data type of 2. So, effectively, data types
-    // 1 and 2 are identical (since they're both bool).
-    int curr_dtype;
-    for(int i = 1; i < array_len; ++i) {
-      curr_dtype = array[i].GetType();
-      if(curr_dtype == 2) {
-        curr_dtype = 1;
-      }
-      if(curr_dtype == 6) {
-        if(array[i].IsDouble()) {
-          curr_dtype = 8;
-        } else {
-          curr_dtype = 9;
-        }
-      }
-      if(curr_dtype != data_type) {
-        list_out = true;
-        break;
-      }
-    }
-    
-    // If the values in the input array are not all of the same data type,
-    // return the array values as an R list.
-    if(list_out) {
+    // If the group of unique data types is longer than two, or if it's length is
+    // two and 0 is NOT one of the two values (0 corresponds to null/NA), then
+    // pass the input array along to array_to_list().
+    if(u_dtypes.size() > 2) {
       return array_to_list<T>(array, array_len);
+    } else if(u_dtypes.size() == 2) {
+      if(u_dtypes[0] == 0) {
+        int dt1 = u_dtypes[1];
+        if(dt1 == 4) {
+          return array_to_list<T>(array, array_len);
+        } else {
+          data_type = dt1;
+        }
+      } else {
+        return array_to_list<T>(array, array_len);
+      }
     }
+    
     
     // Get current value
     switch(data_type) {
@@ -155,7 +142,11 @@ namespace from_json {
     case 1: {
       Rcpp::LogicalVector out(array_len);
       for(int i = 0; i < array_len; ++i) {
-        out[i] = array[i].GetBool();
+        if(array[i].GetType() == 0) {
+          out[i] = NA_LOGICAL;
+        } else {
+          out[i] = array[i].GetBool();
+        }
       }
       return out;
     }
@@ -164,7 +155,11 @@ namespace from_json {
     case 5: {
       Rcpp::CharacterVector out(array_len);
       for(int i = 0; i < array_len; ++i) {
-        out[i] = array[i].GetString();
+        if(array[i].GetType() == 0) {
+          out[i] = NA_STRING;
+        } else {
+          out[i] = array[i].GetString();
+        }
       }
       return out;
     }
@@ -173,7 +168,11 @@ namespace from_json {
     case 8: {
       Rcpp::NumericVector out(array_len);
       for(int i = 0; i < array_len; ++i) {
-        out[i] = array[i].GetDouble();
+        if(array[i].GetType() == 0) {
+          out[i] = NA_REAL;
+        } else {
+          out[i] = array[i].GetDouble();
+        }
       }
       return out;
     }
@@ -182,8 +181,18 @@ namespace from_json {
     case 9: {
       Rcpp::IntegerVector out(array_len);
       for(int i = 0; i < array_len; ++i) {
-        out[i] = array[i].GetInt();
+        if(array[i].GetType() == 0) {
+          out[i] = NA_INTEGER;
+        } else {
+          out[i] = array[i].GetInt();
+        }
       }
+      return out;
+    }
+      
+    // null
+    case 0: {
+      Rcpp::CharacterVector out(array_len, NA_STRING);
       return out;
     }
       
@@ -244,6 +253,12 @@ namespace from_json {
           // int
           out[i] = itr->value.GetInt();
         }
+        break;
+      }
+      
+      // null
+      case 0: {
+        out[i] = Rcpp::CharacterVector(NA_STRING);
         break;
       }
         
@@ -319,6 +334,12 @@ namespace from_json {
         }
         break;
       }
+      
+      // null
+      case 0: {
+        out[i] = Rcpp::CharacterVector(NA_STRING);
+        break;
+      }
         
       // array
       case 4: {
@@ -361,7 +382,11 @@ namespace from_json {
     case 1: {
       Rcpp::LogicalVector out(doc_len);
       for(int i = 0; i < doc_len; ++i) {
-        out[i] = doc[i].GetBool();
+        if(doc[i].GetType() == 0) {
+          out[i] = NA_LOGICAL;
+        } else {
+          out[i] = doc[i].GetBool();
+        }
       }
       return out;
     }
@@ -370,7 +395,11 @@ namespace from_json {
     case 5: {
       Rcpp::CharacterVector out(doc_len);
       for(int i = 0; i < doc_len; ++i) {
-        out[i] = doc[i].GetString();
+        if(doc[i].GetType() == 0) {
+          out[i] = NA_STRING;
+        } else {
+          out[i] = doc[i].GetString();
+        }
       }
       return out;
     }
@@ -379,7 +408,11 @@ namespace from_json {
     case 8: {
       Rcpp::NumericVector out(doc_len);
       for(int i = 0; i < doc_len; ++i) {
-        out[i] = doc[i].GetDouble();
+        if(doc[i].GetType() == 0) {
+          out[i] = NA_REAL;
+        } else {
+          out[i] = doc[i].GetDouble();
+        }
       }
       return out;
     }
@@ -388,8 +421,18 @@ namespace from_json {
     case 9: {
       Rcpp::IntegerVector out(doc_len);
       for(int i = 0; i < doc_len; ++i) {
-        out[i] = doc[i].GetInt();
+        if(doc[i].GetType() == 0) {
+          out[i] = NA_INTEGER;
+        } else {
+          out[i] = doc[i].GetInt();
+        }
       }
+      return out;
+    }
+      
+    // null
+    case 0: {
+      Rcpp::CharacterVector out(doc_len, NA_STRING);
       return out;
     }
     }
@@ -436,6 +479,12 @@ namespace from_json {
           // int
           out[i] = doc[i].GetInt();
         }
+        break;
+      }
+      
+      // null
+      case 0: {
+        out[i] = Rcpp::CharacterVector(NA_STRING);
         break;
       }
         
@@ -491,14 +540,21 @@ namespace from_json {
     // If input in an array, first check the data type of each value in the
     // array.
     Rcpp::IntegerVector dtypes = get_dtypes<rapidjson::Document>(doc);
-    Rcpp::IntegerVector u_dtypes = unique(dtypes);
+    Rcpp::IntegerVector u_dtypes = unique(dtypes).sort();
     
     // If all of the data types are simple and of the same type
     // (i.e. all strings, ints, double, or bool), then return a vector of values.
     if(u_dtypes.size() == 1) {
       int dt = u_dtypes[0];
-      if(dt == 1 || dt == 5 || dt == 8 || dt == 9) {
+      if(dt == 0 || dt == 1 || dt == 5 || dt == 8 || dt == 9) {
         return doc_to_vector(doc, dt);
+      }
+    } else if(u_dtypes.size() == 2) {
+      if(u_dtypes[0] == 0) {
+        int dt1 = u_dtypes[1];
+        if(dt1 == 1 || dt1 == 5 || dt1 == 8 || dt1 == 9) {
+          return doc_to_vector(doc, dt1);
+        }
       }
     }
     
--- tests/testthat/test-from_json.R
+++ tests/testthat/test-from_json.R
@@ -1,9 +1,9 @@
 context("from_json")
 
 test_that("scalar values handled properly", {
-  target <- list(a = 8L, b = 99.5, c = TRUE, d = "cats")
+  target <- list(a = 8L, b = 99.5, c = TRUE, d = "cats", e = NA_character_)
   
-  json_str <- "{\"a\":8, \"b\":99.5, \"c\":true, \"d\":\"cats\"}"
+  json_str <- "{\"a\":8, \"b\":99.5, \"c\":true, \"d\":\"cats\", \"e\":null}"
   expect_equal(from_json(json_str), target)
   
   json_str <- jsonify::to_json(target)
@@ -11,9 +11,9 @@ test_that("scalar values handled properly", {
 })
 
 test_that("vector / array values handled properly", {
-  target <- list(a = c(1L, 2L, 3L), b = list(1L, "cats", 3L))
+  target <- list(a = c(1L, 2L, 3L, NA_integer_), b = list(1L, "cats", 3L, NA_character_))
   
-  json_str <- "{\"a\":[1, 2, 3], \"b\":[1, \"cats\", 3]}"
+  json_str <- "{\"a\":[1, 2, 3, null], \"b\":[1, \"cats\", 3, null]}"
   expect_equal(from_json(json_str), target)
   
   json_str <- jsonify::to_json(target)
@@ -21,9 +21,9 @@ test_that("vector / array values handled properly", {
 })
 
 test_that("nested JSON handled properly", {
-  target <- list(a = 8, b = list(c = 123, d = list(e = 456)))
+  target <- list(a = 8, b = list(c = 123, d = list(e = 456, f = NA_character_)))
   
-  json_str <- "{\"a\":8, \"b\":{\"c\":123, \"d\":{\"e\":456}}}"
+  json_str <- "{\"a\":8, \"b\":{\"c\":123, \"d\":{\"e\":456, \"f\":null}}}"
   expect_equal(from_json(json_str), target)
   
   json_str <- jsonify::to_json(target)
@@ -32,18 +32,18 @@ test_that("nested JSON handled properly", {
 
 test_that("JSON missing keys handled properly", {
   # vector
-  target <- c(1L, 2L, 3L)
+  target <- c(1L, 2L, 3L, NA_integer_)
   
-  json_str <- "[1, 2, 3]"
+  json_str <- "[1, 2, 3, null]"
   expect_equal(from_json(json_str), target)
   
   json_str <- jsonify::to_json(target)
   expect_equal(from_json(json_str), target)
   
   # list
-  target <- list(1L, "cats", 3L)
+  target <- list(1L, "cats", 3L, NA_character_)
   
-  json_str <- "[1, \"cats\", 3]"
+  json_str <- "[1, \"cats\", 3, null]"
   expect_equal(from_json(json_str), target)
   
   json_str <- jsonify::to_json(target)
