c07732428c977615ed18762f4ac79d440ce7ebb6
SymbolixAU <dcooley@symbolix.com.au>
2018-12-18 07:55:49 +1100

    version

--- inst/include/jsonify/to_json/dataframe.hpp
+++ inst/include/jsonify/to_json/dataframe.hpp
@@ -14,7 +14,7 @@ namespace dataframe {
   inline void dataframe_cell( Writer& writer, SEXP& this_vec, size_t& row, 
                               bool unbox = false, int digits = -1 ) {
     
-    //Rcpp::Rcout << "digits: " << digits << std::endl;
+    //Rcpp::Rcout << "JSONIFY digits: " << digits << std::endl;
     
     switch( TYPEOF( this_vec ) ) {
     case VECSXP: {
@@ -64,6 +64,12 @@ namespace dataframe {
       break;
     }
     }
+  }
+  
+  // overload for when you dont' specify 'unbox'
+  template <typename Writer>
+  inline void dataframe_cell( Writer& writer, SEXP& this_vec, size_t& row, int digits = -1 ) {
+    dataframe_cell( writer, this_vec, row, false, digits );
   }
 
   inline Rcpp::StringVector to_json( Rcpp::DataFrame& df, bool unbox = false, int digits = -1 ) {
