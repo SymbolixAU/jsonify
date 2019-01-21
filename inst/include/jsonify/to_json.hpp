db71eab6734f6e766252eec0014e6be8b5667dcb
SymbolixAU <dcooley@symbolix.com.au>
2019-01-22 09:44:50 +1100

    removed unused files - close #36

--- inst/include/jsonify/to_json.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef JSONIFY_TO_JSON_H
-#define JSONIFY_TO_JSON_H
-
-// TODO( remove once spatialwidgets v0.2.0 is on CRAN)
-#include "jsonify/to_json/vectors.hpp"
-
-#endif 
--- inst/include/jsonify/utils.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef R_JSONIFY_WRITERS_UTILS_H
-#define R_JSONIFY_WRITERS_UTILS_H
-
-#include "jsonify/to_json/utils.hpp"
-// TODO( remove once spatialwidget v0.2 is on CRAN)
-#endif 
