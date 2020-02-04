9157263cb3c6cb851ca0bd267be9a734a85fcf58
dcooley <david.cooley141@gmail.com>
2020-02-04 07:41:50 -0800

    fixing my branch mess #61 #58

--- src/validate.cpp
+++ src/validate.cpp
@@ -2,6 +2,8 @@
 #include "jsonify/validate/validate.hpp"
 #include <Rcpp.h>
 
+// temp
+
 // [[Rcpp::export]]
 Rcpp::LogicalVector rcpp_validate_json( Rcpp::StringVector json ) {
   R_xlen_t n = json.size();
