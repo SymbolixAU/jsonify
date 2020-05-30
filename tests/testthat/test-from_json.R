44bcc730990cf5e16f091dc2601cb931ebce1eb3
David <david.cooley141@gmail.com>
2020-05-30 17:19:03 +1000

    tests

--- tests/testthat/test-from_json.R
+++ tests/testthat/test-from_json.R
@@ -423,7 +423,7 @@ test_that("round trips work", {
 
 test_that("UTF-8 encoding is not mangled", {
   
-  testthat::skip()
+  testthat::skip(message = "Tests fail on windows")
   
   test_list <- list(
     id = c("回", "站"),
